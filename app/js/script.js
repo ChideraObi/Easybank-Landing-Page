005b24f98534c3a2e0e0984bc418c76b3fbf544e
Chidera Obinali <chideraobinali@Chideras-MBP.attlocal.net>
2020-12-06 11:01:42 -0500

    Making progress on animated mobile menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,6 +1,7 @@
 const btnHamburger = document.querySelector('#btnHamburger');
 const header = document.querySelector('.header');
 const overlay = document.querySelector('.overlay');
+const fadeElems = document.querySelectorAll('.has-fade');
 
 //mobile menu animation
 btnHamburger.addEventListener('click', function(){
@@ -8,12 +9,16 @@ btnHamburger.addEventListener('click', function(){
     
     if (header.classList.contains('open')){ //close hamburger menu
         header.classList.remove('open');        
-        overlay.classList.add('fade-out');
-        overlay.classList.remove('fade-in');
+        fadeElems.forEach(function(element){
+            element.classList.remove('fade-in');
+            element.classList.add('fade-out');
+        })
     } 
     else { //open hamburger menu
         header.classList.add('open'); 
-        overlay.classList.add('fade-in');
-        overlay.classList.remove('fade-out');
+        fadeElems.forEach(function(element){
+            element.classList.add('fade-in');
+            element.classList.remove('fade-out');
+        })
     }
 });
